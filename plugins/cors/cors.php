eca2063da4ce252e2685404dbfe23e27df45ddf5
Marc Tanis <marcato15@gmail.com>
2017-06-07 06:41:47 -0500

    allow cords

---
+++ plugins/cors/cors.php
@@ -0,0 +1,18 @@
+<?php
+/* Plugin Name: Allow CORS
+Plugin URI: https://horseman.io/
+Description: Allow CORS 
+Version: 1.0
+Author: Marc Tanis 
+Author URI: https://blendimc.com/
+License: GPLv2 or later */
+
+
+add_filter( 'allowed_http_origins', 'add_allowed_origins' );
+function add_allowed_origins( $origins ) {
+    $origins[] = 'http://blend.horseman.dev';
+    $origins[] = 'https://blend.horesman.io';
+    return $origins;
+}
+
+
